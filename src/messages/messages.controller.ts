0e3f25f119004a71b8ae6d974c158572e7a25839
Sebastian Simionescu <contact@sebastian-simionescu.dev>
2023-04-11 20:38:31 +0300

    create new controller and new module

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+
+    @Get()
+    listMessages(){
+
+    }
+
+    @Post()
+    createMessages(@Body() body: any){
+        console.log(body);
+    }
+
+    @Get('/:id')
+    getMessage(@Param('id') id: string){
+        console.log(id);
+    }
+
+}
